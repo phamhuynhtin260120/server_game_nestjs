0269c33b973a9c6a9580917bb1c4c292f72acc07
PhamHuynhTin <phamhuynhtin260120@gmail.com>
2026-05-13 16:17:12 +0700

    add module schedule and tick on game a way

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GameGateway } from './game/game.getway';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [],
+  imports: [ScheduleModule.forRoot()],
   controllers: [AppController],
   providers: [AppService, GameGateway],
 })
--- src/game/game.getway.ts
+++ src/game/game.getway.ts
@@ -4,31 +4,48 @@ import {
     SubscribeMessage,
     OnGatewayConnection,
     OnGatewayDisconnect,
-  } from '@nestjs/websockets';
-  import { Server, Socket } from 'socket.io';
-  
-  @WebSocketGateway({
+} from '@nestjs/websockets';
+import { Interval } from '@nestjs/schedule';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway({
     cors: { origin: '*' }, // Cho phép mọi nguồn kết nối (để tiện test)
-  })
-  export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
+})
+export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
     @WebSocketServer()
     server!: Server;
-  
+
+    private tickCount = 0;
+    @Interval(50)
+    handleGameTick() {
+        this.tickCount++;
+
+        // Giả lập dữ liệu thế giới game
+        const gameState = {
+            serverTime: Date.now(),
+            tick: this.tickCount,
+            entities: [] // Sau này sẽ chứa vị trí người chơi, quái vật...
+        };
+
+        // Gửi trạng thái này tới TẤT CẢ người chơi đang kết nối
+        this.server.emit('worldUpdate', gameState);
+    }
+
     // Khi có một người chơi mới kết nối
     handleConnection(client: Socket) {
-      console.log(`Người chơi kết nối: ${client.id}`);
+        console.log(`Người chơi kết nối: ${client.id}`);
     }
-  
+
     // Khi người chơi thoát game
     handleDisconnect(client: Socket) {
-      console.log(`Người chơi đã rời đi: ${client.id}`);
+        console.log(`Người chơi đã rời đi: ${client.id}`);
     }
-  
+
     // Lắng nghe tin nhắn 'ping' từ Client
     @SubscribeMessage('ping')
     handlePing(client: Socket, data: any) {
-      console.log('Nhận ping từ client:', data);
-      // Phản hồi lại 'pong' kèm thời gian server
-      return { event: 'pong', data: { serverTime: Date.now() } };
+        console.log('Nhận ping từ client:', data);
+        // Phản hồi lại 'pong' kèm thời gian server
+        return { event: 'pong', data: { serverTime: Date.now() } };
     }
-  }
+}
